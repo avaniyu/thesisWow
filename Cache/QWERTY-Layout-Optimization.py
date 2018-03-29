d15c421c6015f67039f5964583b614f9a0db1046
avaniyu <yjybxx2@yahoo.com>
2018-03-29 16:24:30 +0200

    Updating design sketch, WPF pilot.

---
+++ Cache/QWERTY-Layout-Optimization.py
@@ -0,0 +1,8 @@
+# what to consider when grouping QWERTY layout
+# ----------------------------------------
+# English bi-gram frequency
+# ergonomics: put frequent letters in the mid-screen
+# generate multiple layouts with optimization degree ranking, choose a visually appealing one?
+# group several keys == no hole in the key collection graph
+
+
