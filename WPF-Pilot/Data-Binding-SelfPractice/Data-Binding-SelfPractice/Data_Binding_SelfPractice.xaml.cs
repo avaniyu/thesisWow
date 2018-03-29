cab4a686a527a3958c9ad35fb9f6e6fae301a576
avaniyu <yjybxx2@yahoo.com>
2018-03-29 18:18:11 +0200

    Updating WPF pilot data binding practice.

---
+++ WPF-Pilot/Data-Binding-SelfPractice/Data-Binding-SelfPractice/Data_Binding_SelfPractice.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Xml;
+
+namespace Data_Binding_SelfPractice
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class Data_Binding_SelfPractice: Application
+    {
+        private User currentUser;
+        //private ObservableCollection<AuctionItem> auct
+    }
+}
---
+++ WPF-Pilot/Data-Binding-SelfPractice/Data-Binding-SelfPractice/user.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Binding_SelfPractice
+{
+    public class User
+    {
+        private string name;
+        private int rating;
+        private DateTime memberSince;
+
+        #region Property Getters and Setters
+        public string Name
+        {
+            get { return this.name; }
+        }
+        public int Rating
+        {
+            get { return this.rating; }
+            set { this.rating = value; }
+        }
+        public DateTime MemberSince
+        {
+            get { return this.memberSince; }
+        }
+        #endregion
+
+        public User(string name, int rating, DateTime memberSince)
+        {
+            this.name = name;
+            this.rating = rating;
+            this.memberSince = memberSince;
+        }
+    }
+}
