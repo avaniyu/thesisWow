55aa60f0b9ca42352303b84fa1a5a88daf89073f
avaniyu <yjybxx2@yahoo.com>
2018-04-10 18:28:30 +0200

    Deving.

---
+++ Dev/Sample-Codes/CoreSDK-master/samples/OnScreenKeyboard/TermControls/Models/ButtonModel.cs
@@ -0,0 +1,31 @@
+﻿namespace TermControls.Models
+{
+    using System.ComponentModel;
+
+    public class ButtonModel : INotifyPropertyChanged
+    {
+        private string content;
+        public string Content
+        {
+            get
+            {
+                return this.content;
+            }
+            set
+            {
+                this.content = value;
+                this.OnPropertyChanged("Content");
+            }
+        }
+        public string Name { get; set; }
+        public int Column { get; set; }
+
+        #region INotifyPropertyChanged Members
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propertyName)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+    }
+}
