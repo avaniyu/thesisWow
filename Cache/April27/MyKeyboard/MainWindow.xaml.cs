fda018e6fb40c6a9caf81bf24bbb1bec0f369eb5
avaniyu <yjybxx2@yahoo.com>
2018-04-27 18:47:48 +0200

    dev, minimum keyboard works.

---
+++ Cache/April27/MyKeyboard/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MyKeyboard
+{
+    //dependency property register, register attached
+    //    static is because depedency property syntax
+        
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static readonly DependencyProperty TypingResultsProperty = DependencyProperty.Register(
+            "TypingResults",
+            typeof(string),
+            typeof(MainWindow),
+            new UIPropertyMetadata(null));
+
+        public string TypingResults
+        {
+            get { return (string)this.GetValue(TypingResultsProperty); }
+            set { this.SetValue(TypingResultsProperty, value); }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
