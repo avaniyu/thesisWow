6156b1eb0c76d0ce737bd3763a5a226c8179bcce
avaniyu <yjybxx2@yahoo.com>
2019-01-04 10:59:02 +0100

    Clean intermediate-step dev.

--- Cache/QWERTY-Layout-Optimization.py
+++
@@ -1,8 +0,0 @@
-# what to consider when grouping QWERTY layout
-# ----------------------------------------
-# English bi-gram frequency
-# ergonomics: put frequent letters in the mid-screen
-# generate multiple layouts with optimization degree ranking, choose a visually appealing one?
-# group several keys == no hole in the key collection graph
-
-
